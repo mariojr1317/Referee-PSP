8f552295d6bd26070fefe8862a1acab314204cdc
mariojr1317 <20404940@clases.edu.sv>
2026-08-19 20:58:05 -0600

    Add audio.h header file with audio functions

---
+++ audio.h
@@ -0,0 +1,10 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+void emitirPitido(int frecuenciaHz, int duracionMs);
+void sonarSilbato();
+void sonarError();
+void sonarMenu();
+void sonarFinJuego();
+
+#endif
